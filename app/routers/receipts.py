733ca5ab81f6a9cc0b828e3b5787e38950eb26c3
v.drok <v.drok@hicell.tel>
2024-11-16 10:34:13 +0200

    limit and skip

--- app/routers/receipts.py
+++ app/routers/receipts.py
@@ -53,9 +53,25 @@ def get_receipt_preview(id: str, line_length: int = 20, db: Session = Depends(ge
 def get_receipts(
         db: Session = Depends(get_db),
         current_user: User = Depends(verify_access_token),
-        skip: int = 0, search: Optional[str] = ''
+        skip: int = 0,
+        limit: int = 10,
+
+        # date_from: Optional[datetime] = Query(None, description="Filter receipts created after this date"),
+        # date_to: Optional[datetime] = Query(None, description="Filter receipts created before this date"),
+        # min_total: Optional[float] = Query(None, description="Filter receipts with total greater than this amount"),
+        # max_total: Optional[float] = Query(None, description="Filter receipts with total less than this amount"),
+        # payment_type: Optional[str] = Query(None, description="Filter receipts by payment type"),
 ):
-    receipts = db.query(Receipt).filter(Receipt.owner_id == current_user.id)
+    receipts = db.query(Receipt).filter(Receipt.owner_id == current_user.id)\
+        .limit(limit)\
+        .offset(skip)
+
+    # results = db.query(Post, func.count(Vote.post_id).label('votes'))\
+    #     .join(Vote, Vote.post_id == Post.id, isouter=True)\
+    #     .group_by(Post.id)\
+    #     .filter(Post.title.contains(search))\
+    #     .limit(limit)\
+    #     .offset(skip)
 
     if receipts is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No receipts were found for this user")
