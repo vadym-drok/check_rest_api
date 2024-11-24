63eab1cb5213828aee6b97849dd0744b108a76ea
v.drok <v.drok@hicell.tel>
2024-11-24 17:47:33 +0200

    fix create_receipt_preview

--- app/utils.py
+++ app/utils.py
@@ -67,27 +67,38 @@ def get_user_by_username(db: Session, username: str):
 
 
 def create_receipt_preview(receipt: Receipt, line_length: int) -> str:
-    header = "ФОП Джонсонюк Борис".center(line_length)  # TODO -> move to User LegalEntity
-    separator = "=" * line_length
     product_lines = []
     products = receipt.raw_data['products']
-
     last_product = products[-1]
     for product in products:
-        quantity_price = f"{product['quantity']} x {product['price']:.2f}".ljust(line_length // 2)
-        total_price = f"{product['quantity'] * product['price']:.2f}".rjust(line_length // 2)
-        product_name = product['name'][:line_length].ljust(line_length)
-        product_lines.append(f"{quantity_price}{total_price}\n{product_name}")
+        quantity = product.pop('quantity')
+        price = product.pop('price')
+        product_lines.append(f"{quantity} x {price:.2f}")
+
+        name = product.pop('name')
+        product_lines.append(name[:line_length])
+
+        total = product.pop('total')
+        total_price = f"{total:.2f}"
+
+        for add_row_key, add_row_value in product.items():
+            product_lines.append(f"{add_row_key} {add_row_value}")
+
+        last_row = product_lines[-1]
+        spaces_between = ' ' * (line_length - len(last_row) - len(total_price))
+        product_lines[-1] = f"{last_row}{spaces_between}{total_price}"
 
         if product != last_product:
             product_lines.append("-" * line_length)
 
+    header = "ФОП Джонсонюк Борис".center(line_length)  # TODO -> move to User LegalEntity
     total_line = f"СУМА{str(receipt.total):>{line_length - len('СУМА')}}"
     payment_line = f"Картка{str(receipt.amount):>{line_length - len('Картка')}}"
     rest_line = f"Решта{str(receipt.rest):>{line_length - len('Решта')}}"
     footer = "Дякуємо за покупку!".center(line_length)
     date_time = receipt.created_at.strftime("%d.%m.%Y %H:%M").center(line_length)
 
+    separator = "=" * line_length
     receipt_str = f"{header}\n{separator}\n"
     receipt_str += "\n".join(product_lines) + "\n"
     receipt_str += f"{separator}\n{total_line}\n{payment_line}\n{rest_line}\n{separator}\n{date_time}\n{footer}"
