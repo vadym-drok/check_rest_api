b2538f7b0df0538a09de4e0e30bf29f89eb21469
v.drok <v.drok@hicell.tel>
2024-11-26 10:00:02 +0200

    make run-tests

--- Makefile
+++ Makefile
@@ -1,4 +1,2 @@
 run-tests:
 	docker-compose up -d && docker-compose exec web /bin/bash -c 'pytest'
-	#docker-compose up -d
-	#docker-compose exec -it web /bin/bash
--- app/tests/test_base.py
+++ app/tests/test_base.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from app.main import app
+from fastapi import status
 
 
 client = TestClient(app)
@@ -7,4 +8,23 @@ client = TestClient(app)
 
 def test_root():
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_register_user():
+    post_data = {
+        "first_name": "test_first_name",
+        "last_name": "Test_last_name",
+        "username": "test_username_6",
+        "password": "test_password"
+    }
+    response = client.post("/users", json=post_data)
+    response_data = response.json()
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response_data.pop("created_at")
+    assert response_data == {
+        "first_name": "test_first_name",
+        "last_name": "Test_last_name",
+        "username": "test_username_6",
+    }
